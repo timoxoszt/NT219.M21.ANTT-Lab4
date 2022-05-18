65702fd9bb2bd1915d2277193f87a906ff8439b2
timoxoszt <20520810@gm.uit.edu.vn>
2022-05-18 14:25:08 +0700

    Create form

---
+++ HashCalculate/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace HashCalculate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxType = new System.Windows.Forms.ComboBox();
+            this.tbnInput = new System.Windows.Forms.TextBox();
+            this.btnOpenfile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBoxType
+            // 
+            this.comboBoxType.AllowDrop = true;
+            this.comboBoxType.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxType.FormattingEnabled = true;
+            this.comboBoxType.Items.AddRange(new object[] {
+            "Text",
+            "Hex",
+            "File"});
+            this.comboBoxType.Location = new System.Drawing.Point(23, 35);
+            this.comboBoxType.Name = "comboBoxType";
+            this.comboBoxType.Size = new System.Drawing.Size(121, 28);
+            this.comboBoxType.TabIndex = 0;
+            // 
+            // tbnInput
+            // 
+            this.tbnInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbnInput.Location = new System.Drawing.Point(150, 36);
+            this.tbnInput.Name = "tbnInput";
+            this.tbnInput.Size = new System.Drawing.Size(563, 27);
+            this.tbnInput.TabIndex = 1;
+            // 
+            // btnOpenfile
+            // 
+            this.btnOpenfile.Location = new System.Drawing.Point(719, 35);
+            this.btnOpenfile.Name = "btnOpenfile";
+            this.btnOpenfile.Size = new System.Drawing.Size(75, 28);
+            this.btnOpenfile.TabIndex = 2;
+            this.btnOpenfile.Text = "Open";
+            this.btnOpenfile.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnOpenfile);
+            this.Controls.Add(this.tbnInput);
+            this.Controls.Add(this.comboBoxType);
+            this.Name = "Form1";
+            this.Text = "Hash Calculate";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxType;
+        private System.Windows.Forms.TextBox tbnInput;
+        private System.Windows.Forms.Button btnOpenfile;
+    }
+}
+
