9045eb0f74fb85ce9a9c051caeb1ef2caa3e2e6a
timoxoszt <20520810@gm.uit.edu.vn>
2022-05-18 16:13:19 +0700

    text and hex input

--- HashCalculate/Form1.Designer.cs
+++ HashCalculate/Form1.Designer.cs
@@ -35,7 +35,7 @@ namespace HashCalculate
             this.tbMD5 = new System.Windows.Forms.TextBox();
             this.tbSHA1 = new System.Windows.Forms.TextBox();
             this.tbSHA3 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnCalculate = new System.Windows.Forms.Button();
             this.cbMD5 = new System.Windows.Forms.CheckBox();
             this.cbSHA1 = new System.Windows.Forms.CheckBox();
             this.cbSHA3 = new System.Windows.Forms.CheckBox();
@@ -52,69 +52,75 @@ namespace HashCalculate
             "Text",
             "Hex",
             "File"});
-            this.comboBoxType.Location = new System.Drawing.Point(17, 28);
-            this.comboBoxType.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.comboBoxType.Location = new System.Drawing.Point(23, 34);
+            this.comboBoxType.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.comboBoxType.Name = "comboBoxType";
-            this.comboBoxType.Size = new System.Drawing.Size(92, 25);
+            this.comboBoxType.Size = new System.Drawing.Size(121, 28);
             this.comboBoxType.TabIndex = 0;
             // 
             // data
             // 
             this.data.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.data.Location = new System.Drawing.Point(112, 29);
-            this.data.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.data.Location = new System.Drawing.Point(149, 36);
+            this.data.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.data.Name = "data";
-            this.data.Size = new System.Drawing.Size(423, 23);
+            this.data.Size = new System.Drawing.Size(563, 27);
             this.data.TabIndex = 1;
             // 
             // btnOpenfile
             // 
-            this.btnOpenfile.Location = new System.Drawing.Point(539, 28);
-            this.btnOpenfile.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnOpenfile.Location = new System.Drawing.Point(719, 34);
+            this.btnOpenfile.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnOpenfile.Name = "btnOpenfile";
-            this.btnOpenfile.Size = new System.Drawing.Size(56, 23);
+            this.btnOpenfile.Size = new System.Drawing.Size(75, 28);
             this.btnOpenfile.TabIndex = 2;
             this.btnOpenfile.Text = "Open";
             this.btnOpenfile.UseVisualStyleBackColor = true;
             // 
             // tbMD5
             // 
-            this.tbMD5.Location = new System.Drawing.Point(112, 120);
+            this.tbMD5.Location = new System.Drawing.Point(149, 148);
+            this.tbMD5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tbMD5.Name = "tbMD5";
-            this.tbMD5.Size = new System.Drawing.Size(423, 20);
+            this.tbMD5.Size = new System.Drawing.Size(563, 22);
             this.tbMD5.TabIndex = 3;
             // 
             // tbSHA1
             // 
-            this.tbSHA1.Location = new System.Drawing.Point(112, 189);
+            this.tbSHA1.Location = new System.Drawing.Point(149, 233);
+            this.tbSHA1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tbSHA1.Name = "tbSHA1";
-            this.tbSHA1.Size = new System.Drawing.Size(423, 20);
+            this.tbSHA1.Size = new System.Drawing.Size(563, 22);
             this.tbSHA1.TabIndex = 4;
             // 
             // tbSHA3
             // 
-            this.tbSHA3.Location = new System.Drawing.Point(112, 268);
+            this.tbSHA3.Location = new System.Drawing.Point(149, 330);
+            this.tbSHA3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tbSHA3.Name = "tbSHA3";
-            this.tbSHA3.Size = new System.Drawing.Size(423, 20);
+            this.tbSHA3.Size = new System.Drawing.Size(563, 22);
             this.tbSHA3.TabIndex = 5;
             // 
-            // button1
+            // btnCalculate
             // 
-            this.button1.BackColor = System.Drawing.Color.Silver;
-            this.button1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(248, 316);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(147, 38);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "CALCULATE";
-            this.button1.UseVisualStyleBackColor = false;
+            this.btnCalculate.BackColor = System.Drawing.Color.Silver;
+            this.btnCalculate.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(331, 389);
+            this.btnCalculate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(196, 47);
+            this.btnCalculate.TabIndex = 6;
+            this.btnCalculate.Text = "CALCULATE";
+            this.btnCalculate.UseVisualStyleBackColor = false;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
             // 
             // cbMD5
             // 
             this.cbMD5.AutoSize = true;
-            this.cbMD5.Location = new System.Drawing.Point(33, 123);
+            this.cbMD5.Location = new System.Drawing.Point(44, 151);
+            this.cbMD5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cbMD5.Name = "cbMD5";
-            this.cbMD5.Size = new System.Drawing.Size(49, 17);
+            this.cbMD5.Size = new System.Drawing.Size(59, 21);
             this.cbMD5.TabIndex = 7;
             this.cbMD5.Text = "MD5";
             this.cbMD5.UseVisualStyleBackColor = true;
@@ -122,9 +128,10 @@ namespace HashCalculate
             // cbSHA1
             // 
             this.cbSHA1.AutoSize = true;
-            this.cbSHA1.Location = new System.Drawing.Point(33, 192);
+            this.cbSHA1.Location = new System.Drawing.Point(44, 236);
+            this.cbSHA1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cbSHA1.Name = "cbSHA1";
-            this.cbSHA1.Size = new System.Drawing.Size(54, 17);
+            this.cbSHA1.Size = new System.Drawing.Size(66, 21);
             this.cbSHA1.TabIndex = 8;
             this.cbSHA1.Text = "SHA1";
             this.cbSHA1.UseVisualStyleBackColor = true;
@@ -132,9 +139,10 @@ namespace HashCalculate
             // cbSHA3
             // 
             this.cbSHA3.AutoSize = true;
-            this.cbSHA3.Location = new System.Drawing.Point(33, 271);
+            this.cbSHA3.Location = new System.Drawing.Point(44, 334);
+            this.cbSHA3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cbSHA3.Name = "cbSHA3";
-            this.cbSHA3.Size = new System.Drawing.Size(54, 17);
+            this.cbSHA3.Size = new System.Drawing.Size(66, 21);
             this.cbSHA3.TabIndex = 9;
             this.cbSHA3.Text = "SHA3";
             this.cbSHA3.UseVisualStyleBackColor = true;
@@ -142,41 +150,44 @@ namespace HashCalculate
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(17, 13);
+            this.label1.Location = new System.Drawing.Point(23, 16);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.Size = new System.Drawing.Size(86, 17);
             this.label1.TabIndex = 10;
             this.label1.Text = "Data Format";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(112, 13);
+            this.label2.Location = new System.Drawing.Point(149, 16);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.Size = new System.Drawing.Size(38, 17);
             this.label2.TabIndex = 11;
             this.label2.Text = "Data";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.cbSHA3);
             this.Controls.Add(this.cbSHA1);
             this.Controls.Add(this.cbMD5);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnCalculate);
             this.Controls.Add(this.tbSHA3);
             this.Controls.Add(this.tbSHA1);
             this.Controls.Add(this.tbMD5);
             this.Controls.Add(this.btnOpenfile);
             this.Controls.Add(this.data);
             this.Controls.Add(this.comboBoxType);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Hash Calculate";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -190,7 +201,7 @@ namespace HashCalculate
         private System.Windows.Forms.TextBox tbMD5;
         private System.Windows.Forms.TextBox tbSHA1;
         private System.Windows.Forms.TextBox tbSHA3;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnCalculate;
         private System.Windows.Forms.CheckBox cbMD5;
         private System.Windows.Forms.CheckBox cbSHA1;
         private System.Windows.Forms.CheckBox cbSHA3;
--- HashCalculate/Form1.cs
+++ HashCalculate/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Security.Cryptography;
+using HashLib;
 
 namespace HashCalculate
 {
@@ -15,6 +17,111 @@ namespace HashCalculate
         public Form1()
         {
             InitializeComponent();
+        }
+        string md5_output;
+        string sha1_output;
+        string sha256_output;
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            string input = data.Text;
+
+            switch (comboBoxType.Text.ToString())
+            {
+                case "Text":
+                    md5_output = TextMd5Hash(input);
+                    sha1_output = TextSHA1Hash(input);
+                    sha256_output = TextSha256Hash(input);
+                    break;
+                case "Hex":
+                    md5_output = HexMd5Hash(input);
+                    sha1_output = HexSHA1Hash(input);
+                    sha256_output = HexSha256Hash(input);
+                    break;
+                case "File":
+                    md5_output = "hehe";
+                    break;
+            }
+            tbMD5.Text = md5_output;
+            tbSHA1.Text = sha1_output;
+            tbSHA3.Text = sha256_output;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBoxType.SelectedIndex = 0;
+        }
+
+        public static string TextMd5Hash(string message)
+        {
+            using (MD5 md5 = MD5.Create())
+            {
+                byte[] input = Encoding.UTF8.GetBytes(message);
+                byte[] hash = md5.ComputeHash(input);
+
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < hash.Length; i++)
+                {
+                    sb.Append(hash[i].ToString("X2")); // print in hexadecimal format
+                }
+                return sb.ToString().ToLower();
+            }
+        }
+
+        public static string HextoString(string message)
+        {
+            byte[] hts = Enumerable.Range(0, message.Length)
+                     .Where(x => x % 2 == 0)
+                     .Select(x => Convert.ToByte(message.Substring(x, 2), 16))
+                     .ToArray();
+            return Encoding.ASCII.GetString(hts);
+        }
+
+        public static string HexMd5Hash(string message)
+        {
+            return TextMd5Hash(HextoString(message));
+        }
+
+        static string TextSHA1Hash(string message)
+        {
+            using (SHA1Managed sha1 = new SHA1Managed())
+            {
+                var hash = sha1.ComputeHash(Encoding.UTF8.GetBytes(message));
+                var sb = new StringBuilder(hash.Length * 2);
+
+                foreach (byte b in hash)
+                {
+                    sb.Append(b.ToString("X2"));
+                }
+
+                return sb.ToString().ToLower();
+            }
+        }
+
+        public static string HexSHA1Hash(string message)
+        {
+            return TextSHA1Hash(HextoString(message));
+        }
+
+        static string TextSha256Hash(string message)
+        {
+            var hashAlgorithm = new Org.BouncyCastle.Crypto.Digests.Sha3Digest(256);
+            byte[] input = Encoding.ASCII.GetBytes(message);
+
+            hashAlgorithm.BlockUpdate(input, 0, input.Length);
+
+            byte[] result = new byte[32]; // 256 / 8 = 32
+            hashAlgorithm.DoFinal(result, 0);
+
+            string hashString = BitConverter.ToString(result);
+            hashString = hashString.Replace("-", "").ToLowerInvariant();
+
+            return hashString;
+        }
+
+        static string HexSha256Hash(string message)
+        {
+            return TextSha256Hash(HextoString(message));
         }
     }
 }
